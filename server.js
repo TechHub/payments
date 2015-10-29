0ccf10de5c57462ea901e1310ed131d92a89c205
Mattia Asti <mattia.asti@gmail.com>
2015-10-29 16:30:11 +0000

    first version

--- server.js
+++ server.js
@@ -3,30 +3,31 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 
-const stripe = require("stripe")(
-  "sk_test_melwcmbGnk2eOxtdmidxJTnB"
-);
+const stripeKey = process.env.STRIPE_KEY;
 
-app.use(cors());
+const stripe = require("stripe")(stripeKey);
+
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.static('public'));
 
 app.post('/charge', (req, res) => {
   console.log(req.body);
   const chargeParams = {
-    amount: 2000,
+    amount: req.body.amount,
     currency: 'gbp',
-    source: req.body.id, // obtained with Stripe.js
-    description: 'Charge for test@example.com'
+    source: req.body.token, // obtained with Stripe.js
+    description: req.body.description,
+    receipt_email: req.body.email
   };
-    stripe.charges.create(chargeParams, (err, charge) => {
-      // asynchronously called
-      console.log(err, charge);
-    });
-  res.send('okay');
+  stripe.charges.create(chargeParams, (err, charge) => {
+    // asynchronously called
+    console.log(err, charge);
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.send(charge);
+  });
 });
 
 
