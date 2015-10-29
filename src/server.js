388a075304b26eab698b907f55df0075a181b10c
Mattia Asti <mattia.asti@gmail.com>
2015-10-29 16:40:35 +0000

    added Babel

--- src/server.js
+++ src/server.js
@@ -30,10 +30,4 @@ app.post('/charge', (req, res) => {
   });
 });
 
-
-const server = app.listen(process.env.PORT || 3000, () => {
-  const host = server.address().address;
-  const port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+module.exports = app;
