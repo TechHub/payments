45766080560c5a614332f83426051e6d2b4cf819
Mattia <mattia.asti@gmail.com>
2017-07-03 12:21:20 +0100

    removed warsaw madrid

--- src/server.js
+++ src/server.js
@@ -1,28 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const braintree = require('braintree');
 const RazorNode = require('razornode').razorNode;
 const cors = require('cors');
 const app = express();
-let braintreeEnv;
-
-if (process.env.NODE_ENV === 'production') {
-  braintreeEnv = braintree.Environment.Production;
-} else {
-  braintreeEnv = braintree.Environment.Sandbox;
-}
 
 // CONFIG
 const stripeLondon = require('stripe')(process.env.STRIPE_KEY_LONDON);
-const stripeMadrid = require('stripe')(process.env.STRIPE_KEY_MADRID);
 const razorPay = new RazorNode(process.env.RAZOR_KEY, process.env.RAZOR_SECRET);
-
-const braintreeGateway = braintree.connect({
-  environment: braintreeEnv,
-  merchantId: process.env.BRAINTREE_MERCHANT_ID,
-  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
-  privateKey: process.env.BRAINTREE_PRIVATE_KEY,
-});
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -37,48 +21,8 @@ app.get('/london', (req, res) => {
   res.render('london.ejs', {});
 });
 
-app.get('/madrid', (req, res) => {
-  res.render('madrid.ejs', {});
-});
-
-app.get('/warsaw', (req, res) => {
-  braintreeGateway.clientToken.generate({}, (err, response) => {
-    res.render('warsaw.ejs', {
-      braintreeToken: response.clientToken,
-    });
-  });
-});
-
 app.get('/bangalore', (req, res) => {
   res.render('bangalore.ejs', {});
-});
-
-app.get('/braintree_token', (req, res) => {
-  braintreeGateway.clientToken.generate({}, (err, response) => {
-    res.send(response);
-  });
-});
-
-app.post('/charge_braintree', (req, res) => {
-  braintreeGateway.transaction.sale({
-    amount: req.body.amount,
-    paymentMethodNonce: req.body.nonce,
-    customer: {
-      firstName: req.body.givenName,
-      lastName: req.body.surname,
-      company: req.body.company,
-      email: req.body.email,
-    },
-    customFields: {
-      description: req.body.description,
-    },
-    options: {
-      submitForSettlement: true,
-    },
-  }, (err, result) => {
-    console.log(err);
-    res.send(result);
-  });
 });
 
 app.post('/charge_stripe', (req, res) => {
